afe4b35a318a1b023fe4f5af696cd6c99ed6cfc3
Nik-Lumos <119259668+Nik-Lumos@users.noreply.github.com>
2022-12-12 17:04:35 +0300

    Лабораторная работа 5 (попытка 2)

---
+++ Лабораторная работа 5/L5_task2.py
@@ -0,0 +1,17 @@
+import random
+
+
+def get_unique_list_numbers() -> list[int]:
+    beautiful_list = []  # Будущий список
+    # Запускаем цикл, в котором генерируем новые числа, пока не получим число, которого еще нет в списке
+    while len(beautiful_list) < 15:
+        new_number = random.randint(-10, 10)
+        if beautiful_list.count(new_number) == 0:
+            beautiful_list += [new_number]  # Добавляем число в список
+
+    return beautiful_list
+
+
+list_unique_numbers = get_unique_list_numbers()
+print(list_unique_numbers)
+print(len(list_unique_numbers) == len(set(list_unique_numbers)))
